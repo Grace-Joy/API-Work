51251c6da74e3366468229fd2b60e18ad1cab698
Grace-Joy <33892212+Grace-Joy@users.noreply.github.com>
2019-05-30 17:03:04 +0300

    Add files via upload

---
+++ Screencaster-3b.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$numbers = array("20.00", "15", "6"); 
+echo "Engineer-Wairuri-Kamau|" . $numbers[0] . ", " . $numbers[1] . " and " . $numbers[2] . ".";
+?>
+
+</body>
+</html>
